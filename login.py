76f3d7e4c62901442bc78ca46b39027d79011979
pjluispb <124328519+pjluispb@users.noreply.github.com>
2024-01-18 14:16:34 -0400

    Add files via upload

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from deta import Deta
 import pandas as pd
-import time
+from streamlit_option_menu import option_menu
 
 st.set_page_config(
     page_title="ASIGLEH app",
@@ -15,45 +15,23 @@ st.set_page_config(
 deta = Deta(st.secrets["deta_key"])
 encprof = deta.Base('asiglehpastores')
 
-st.header('Iglesia de Los Hermanos - Venezuela')
+st.header('App3M')
 st.markdown("<h4 style='text-align: left; color: grey;'>ASIGLEH</h4>", unsafe_allow_html=True)
 st.subheader('Monitoreo y Mentoreo a Ministros y Líderes')
 
-#st.write('Elaborado por APTA - Dtto Andino')
+selected3 = option_menu(None, ["", "Ingresar", "Registro"], 
+    icons=['hourglass','door-open-fill', 'person-rolodex'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#3B3131"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
+selected3
+if selected3=='Registro':
+    st.toast('Módulo en Construcción')
+elif selected3=='Ingresar':
+    switch_page('A-login1')
 
-#st.markdown('Bienvenido al Sistema etc etc')
-
-st.markdown("<h1 style='text-align: center; color: grey;'>Login</h1>", unsafe_allow_html=True)
-col1,col2,col3 = st.columns(3)
-with col2:
-    with st.form('fregini'):
-        st.markdown("<h3 style='text-align: center; color: red;'>Usuario </h2>", unsafe_allow_html=True)
-        nombreu = st.text_input(label='nombreu', label_visibility='collapsed')
-        st.markdown("<h3 style='text-align: center; color: red;'>Cédula </h2>", unsafe_allow_html=True)
-        cedulau = st.text_input(label='cedulau', label_visibility='collapsed')
-        streg, nom, ced = True, nombreu, cedulau
-        ingresaReg = st.form_submit_button('Enviar')
-        if ingresaReg:
-            verifregDB = encprof.fetch({"nombreu":nombreu, "cedulau":cedulau})
-            if verifregDB.count==1:
-                st.session_state['nombreu'] = nombreu
-                st.session_state['cedulau'] = cedulau
-                st.session_state['clave'] = verifregDB.items[0]['key']
-                st.session_state['rol'] = verifregDB.items[0]['rol']
-                st.write(verifregDB.count)
-                st.write(verifregDB.items)
-                #st.stop()
-                if st.session_state['rol']=='Pastor':
-                    # switch_page('encupast02')
-                    switch_page('encupast02mir')
-                    # switch_page('encupast02edit_ver2')
-                else:
-                    switch_page('enculidAsigleh')
-                    #switch_page('testnewitem')
-                
-            else:
-                st.toast('Error en los datos ingresados')
-                st.toast('Intente de nuevo')
-                #st.error('''
-                #         Error en los datos ingresados.
-                #         Intente de nuevo''')
